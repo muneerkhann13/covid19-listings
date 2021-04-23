17fca7a056059715520ab43fc2675558717a39b1
Mohd. Muneer Arif <muneerkhann13@gmail.com>
2021-04-23 22:00:45 +0530

    table added

--- src/main/resources/db/migration/V000001__CreateTableSample.sql
+++ src/main/resources/db/migration/V000001__CreateTableSample.sql
@@ -1,15 +1,24 @@
 create database if not exists sample;
 use sample;
 
-CREATE TABLE `sample` (
+CREATE TABLE `Listing` (
   `id`         BIGINT(20)   NOT NULL AUTO_INCREMENT,
-  `name`       VARCHAR(50)           DEFAULT NULL,
-  `created_by` VARCHAR(50)           NOT NULL,
+  `category`       VARCHAR(50)            NOT NULL,
+  `city`       VARCHAR(50)            NOT NULL,
+  `state`       VARCHAR(50)            NOT NULL,
+  `country`       VARCHAR(50)            NOT NULL,
+  `info`       VARCHAR(200)           DEFAULT NULL,
+  `Address`       VARCHAR(500)            NOT NULL,
+  `contact_number`       VARCHAR(200)            NOT NULL,
+  `user_id` BIGINT(20)           NOT NULL,
   `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE (`name`),
-  KEY idx_updated_on (updated_on)
-)
+  KEY idx_updated_on (updated_on),
+  KEY idx_category (category),
+  KEY idx_state (state),
+  KEY idx_city (city),
+  KEY idx_category_state_city (category,state,city)
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
